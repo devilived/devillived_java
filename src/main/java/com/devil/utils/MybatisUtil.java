92cc8e81b06e1fda956faf4547bd88845e57d705
xqs <xingqisheng@vidmt.com>
2016-04-01 13:21:03 +0800

    增加mybatisutil中对null的支持

--- src/main/java/com/devil/utils/MybatisUtil.java
+++ src/main/java/com/devil/utils/MybatisUtil.java
@@ -2,8 +2,8 @@ package com.devil.utils;
 
 public class MybatisUtil {
 	public static void main(String[] args) {
-		String sql = "SELECT uid,lon,lat,`time`, 6378.138*1000*2*asin(sqrt(pow(sin( (?-lat)*pi()/180/2),2)+cos(?*pi()/180)*cos(lat*pi()/180)* pow(sin((?-lon)*pi()/180/2),2))) AS distance FROM v_location_nearby AS a WHERE a.loc_private=0 AND a.uid != ? ORDER BY distance ASC limit ?,? ";
-		String params = "28.68062(Double), 28.68062(Double), 112.900653(Double), 2498024(Long), 0(Integer), 20(Integer)";
+		String sql = "INSERT INTO v_user(id,`name`,nick,email,phone,sex,birth,status,create_date) VALUES(?,?,?,?,?,?,?,?,now())";
+		String params = "2562551(Long), null, null, null, 12345678901(String), 0(Integer), 1996-04-06(Date), NORMAL(String)";
 		System.out.println(buildSql(sql, params));
 	}
 
@@ -12,12 +12,16 @@ public class MybatisUtil {
 
 		for (int i = 0; i < paramarr.length; i++) {
 			String rawparam = paramarr[i].trim();
-			String value = rawparam.substring(0, rawparam.indexOf("("));
-			String type = rawparam.substring(rawparam.indexOf("(") + 1, rawparam.length() - 1);
-			if (notNeedQuot(type)) {
-				paramarr[i] = value;
+			if ("null".equals(rawparam)) {
+				paramarr[i] = rawparam;
 			} else {
-				paramarr[i] = " '" + value + "' ";
+				String value = rawparam.substring(0, rawparam.indexOf("("));
+				String type = rawparam.substring(rawparam.indexOf("(") + 1, rawparam.length() - 1);
+				if (notNeedQuot(type)) {
+					paramarr[i] = value;
+				} else {
+					paramarr[i] = " '" + value + "' ";
+				}
 			}
 		}
 		return String.format(statement.replace("?", "%s"), paramarr);
