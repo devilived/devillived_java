84668afe5fed9d532485a6fb38ea2c41832cc70a
xqs <xingqisheng@vidmt.com>
2016-06-01 10:46:25 +0800

    aes

--- src/main/java/com/devil/des/DESCoder.java
+++ src/main/java/com/devil/des/DESCoder.java
@@ -20,7 +20,6 @@ import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -28,7 +27,7 @@ import org.dom4j.io.OutputFormat;
 import org.dom4j.io.SAXReader;
 import org.dom4j.io.XMLWriter;
 
-import com.devil.utils.Base64Coder;
+import com.devil.utils.CommUtil;
 
 /* 
  * @Author xingqisheng
@@ -43,24 +42,33 @@ public class DESCoder {
 	// private byte[] paramvector = { 1, 2, 3, 4, 5, 6, 7, 8 };
 	private byte[] paramvector = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
+	private static DESCoder defaultInstance;
+
+	public static DESCoder getDefault() {
+		if (defaultInstance == null) {
+			defaultInstance = new DESCoder();
+		}
+		return defaultInstance;
+	}
+
+	private DESCoder() {
+		this.key = this.getPrivateKey();
+		paramvector = Arrays.copyOfRange(key, 0, 8);
+	}
+
 	public DESCoder(byte[] key) {
 		this.key = key;
 		paramvector = Arrays.copyOfRange(key, 0, 8);
 	}
 
-	public DESCoder() {
-		this.key = this.getPrivateKey();
-		paramvector = Arrays.copyOfRange(key, 0, 8);
-	}
-
 	public static void main(String[] args) {
-//		String src = "guessyourheart";
-//		System.out.println("原文是:" + src);
-//		byte[] key = "guessyourheart".getBytes(Charset.forName("UTF-8"));
-		String src ="https://raw.github.com/configs/configs/master/guessyourheart";
+		// String src = "guessyourheart";
+		// System.out.println("原文是:" + src);
+		// byte[] key = "guessyourheart".getBytes(Charset.forName("UTF-8"));
+		String src = "https://raw.github.com/configs/configs/master/guessyourheart";
 		System.out.println("原文是:" + src);
 		byte[] key = "12345678".getBytes(Charset.forName("UTF-8"));
-		DESCoder coder = new DESCoder(key);
+		DESCoder coder = new DESCoder();
 		String cypher = coder.encrypt(src.getBytes());
 		System.out.println("加密后:" + new String(cypher));
 		byte[] plain = coder.decrypt(cypher);
@@ -68,18 +76,19 @@ public class DESCoder {
 
 		// System.out.println("======测试xml文件加密=======");
 		// String testElName="username";
-		// coder.encryptXml("C:\\Documents and Settings\\Administrator\\v3\\conf\\v3-taobao-config.xml");
+		// coder.encryptXml("C:\\Documents and
+		// Settings\\Administrator\\v3\\conf\\v3-taobao-config.xml");
 		// System.out.println("xml原文是:"+XmlUtil.getDocument("d:\\xx.xml").getRootElement().elementText(testElName));
 		// Document doc = coder.decryptXml("d:\\xx_new.xml");
 		// System.out.println("xml解密后:"+doc.getRootElement().elementText(testElName));
 	}
 
 	public String encrypt(byte[] src) {
-		return Base64Coder.encode(this.desEncrypt(src)).replace("\n", "");
+		return CommUtil.encBase64Url(this.desEncrypt(src));
 	}
 
 	public byte[] decrypt(String src) {
-		return this.desDecrypt(Base64Coder.decode(src));
+		return this.desDecrypt(CommUtil.decBase64Url(src));
 	}
 
 	// 作为一个工具使用
@@ -108,13 +117,17 @@ public class DESCoder {
 		byte[] result = null;
 		try {
 			byte[] bArr = { 100, 101, 118, 105, 108, 105, 118, 101, 100 };
-			SecureRandom secureRandom = new SecureRandom(bArr);
+			SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+			secureRandom.setSeed(bArr);
+
+			// SecureRandom secureRandom = new SecureRandom(bArr);
 			KeyGenerator kg = null;
 			kg = KeyGenerator.getInstance(ALGORITHM);
 			kg.init(secureRandom);
 
 			SecretKey secretKey = kg.generateKey();
 			result = secretKey.getEncoded();
+			System.out.println(Arrays.toString(result));
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
@@ -174,8 +187,7 @@ class XmlUtil {
 		if (f.exists()) {
 			SAXReader reader = new SAXReader();
 			try {
-				doc = reader.read(new InputStreamReader(new FileInputStream(f),
-						"UTF-8"));
+				doc = reader.read(new InputStreamReader(new FileInputStream(f), "UTF-8"));
 			} catch (Exception e) {
 				System.out.println("读入Xml文件" + path + "有误");
 				e.printStackTrace();
@@ -188,8 +200,7 @@ class XmlUtil {
 		OutputFormat format = OutputFormat.createPrettyPrint();
 		XMLWriter writer = null;
 		try {
-			writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(
-					destPah), "UTF-8"), format);
+			writer = new XMLWriter(new OutputStreamWriter(new FileOutputStream(destPah), "UTF-8"), format);
 			writer.write(doc);
 		} catch (Exception e) {
 		} finally {
@@ -207,9 +218,7 @@ class XmlUtil {
 	protected static void encryptElement(Element e, DESCoder coder) {
 		String text = e.getTextTrim();
 		if (text != null && text.length() > 0) {
-			if (e.attribute("encrypt") != null
-					&& "true"
-							.equalsIgnoreCase(e.attribute("encrypt").getText())) {
+			if (e.attribute("encrypt") != null && "true".equalsIgnoreCase(e.attribute("encrypt").getText())) {
 				try {
 					text = coder.encrypt(text.getBytes("UTF-8"));
 					e.setText(text);
@@ -231,9 +240,7 @@ class XmlUtil {
 	protected static void decryptElement(Element e, DESCoder coder) {
 		String text = e.getTextTrim();
 		if (text != null && text.length() > 0) {
-			if (e.attribute("encrypt") != null
-					&& "true"
-							.equalsIgnoreCase(e.attribute("encrypt").getText())) {
+			if (e.attribute("encrypt") != null && "true".equalsIgnoreCase(e.attribute("encrypt").getText())) {
 				try {
 					text = new String(coder.decrypt(text), "UTF-8");
 					e.setText(text);
